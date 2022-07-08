21873222bbdcb2516d18faadae4e6abf9b5d23d2
Will-Fash <wl850932@dal.ca>
2022-07-08 18:36:48 -0300

    Add changes to Followers

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -8,6 +8,15 @@ const Followers = () => {
         <div className="followers">
             {followers.map((follower, index) => {
                 const {avatar_url:img, html_url, login} = follower;
+                return (
+                    <article key={index}>
+                        <img  src={img} alt={login} />
+                        <div>
+                            <h4>{login}</h4>
+                            <a href={html_url}>{html_url}</a>
+                        </div>
+                    </article>
+                );
             })}
         </div>
     </Wrapper>
